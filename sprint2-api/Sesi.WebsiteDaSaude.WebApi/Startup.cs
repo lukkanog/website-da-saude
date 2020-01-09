c286aadc3ed453f1226818d8731f8814c94e7fe4
lukkanog <lukanog@hotmail.com>
2020-01-09 02:52:36 -0300

    startup configurada, falta scaffold do EFCore

--- sprint2-api/Sesi.WebsiteDaSaude.WebApi/Startup.cs
+++ sprint2-api/Sesi.WebsiteDaSaude.WebApi/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Sesi.WebsiteDaSaude.WebApi 
 {
