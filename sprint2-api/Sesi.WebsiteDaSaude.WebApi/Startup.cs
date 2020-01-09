c9eb44170771ccd32bf6817e5942e8b8446edc12
lukkanog <lukanog@hotmail.com>
2020-01-09 01:45:29 -0300

    :hatching_chick: API criada e configurada

---
+++ sprint2-api/Sesi.WebsiteDaSaude.WebApi/Startup.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace Sesi.WebsiteDaSaude.WebApi 
+{
+    public class Startup 
+    {
+        public Startup (IConfiguration configuration) 
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices (IServiceCollection services) {
+            //mvc
+            services.AddMvc ()
+                .AddJsonOptions (options => {
+                    options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+                    options.SerializerSettings.NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore;
+                })
+                .SetCompatibilityVersion (Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_2_2);
+
+            //    .SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_2_1);
+
+
+            //swagger
+            services.AddSwaggerGen (c =>
+                c.SwaggerDoc ("v1",
+                    new Swashbuckle.AspNetCore.Swagger.Info 
+                    {
+                        Title = "Website Da Saúde API",
+                        Version = "v1"
+                    })
+            );
+
+            //authentication
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = "JwtBearer";
+                options.DefaultChallengeScheme = "JwtBearer";
+            }).AddJwtBearer("JwtBearer", options =>
+            {
+                options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+
+                    ValidateAudience = true,
+
+                    ValidateLifetime = true,
+
+                    IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("websiteDaSaude-chave-autenticacao")),
+
+                    ClockSkew = TimeSpan.FromDays(30),
+
+                    ValidIssuer = "WebsiteDaSaude.WebApi",
+
+                    ValidAudience = "WebsiteDaSaude.WebApi"
+                };
+            });
+
+            //CORS (Cross origin resource sharing)
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader()
+                        .AllowCredentials());
+            });
+
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure (IApplicationBuilder app, IHostingEnvironment env) 
+        {
+            if (env.IsDevelopment ()) 
+            {
+                app.UseDeveloperExceptionPage ();
+            } else 
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts ();
+            }
+
+            app.UseHttpsRedirection ();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => 
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Website Da Saúde API");
+            });
+            app.UseMvc ();
+        }
+    }
+}
