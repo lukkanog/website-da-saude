66ae6cb5545b7831199f8b671390fb1d8cbcb899
lukkanog <lukanog@hotmail.com>
2020-08-04 14:29:31 -0300

    cadastro de usuarios criado (sem estilizacao)

---
+++ frontend/portalDaSaude-frontend/js/admUsuarios.js
@@ -0,0 +1,134 @@
+const selectBairro = document.querySelector("#bairro");
+const form = document.querySelector("#cadastrar_usuario");
+// // const section = document.querySelector("#locais_cadastrados");
+// const loading = document.querySelector("#loading");
+
+carregarBairros();
+$("#cep").mask("99999-999");
+
+
+cadastrarUsuario = async(event) => {
+    event.preventDefault();
+
+
+    if ($("#senha").val() !== $("#confirmar_senha").val()) {
+        alert("Por favor, confirme sua senha corretamente")
+    } else {
+
+        // IMPORTANTE DEIXAR O IDPERMISSAO COMO 2
+        const idPermissao = 2;
+
+
+        let requestBody = {
+            idPermissao: idPermissao,
+            nomeUsuario: $("#nome_usuario").val(),
+            dataNascimento: $("#nascimento_usuario").val(),
+            email: $("#email").val(),
+            senha: $("#senha").val(),
+            idBairro : $("#bairro").val(),
+            cep: $("#cep").val() === "" ? null : $("#cep").val(),
+            logradouro: $("#logradouro").val() === "" ? null : $("#logradouro").val(),
+            numero: $("#numero").val(),
+        }
+
+        let token = localStorage.getItem("portalDaSaude-token");
+        const url = "http://localhost:5000/api/usuarios";
+        console.log(requestBody);
+
+        fetch(url,{
+            method: "POST",
+            headers: {
+                "Authorization": "Bearer " + token,
+                "Content-Type" : "application/json",
+            },
+            body : JSON.stringify(requestBody)
+        })
+        .then(resposta => resposta.json())
+        .then(data => alert(data.mensagem))
+        .catch(error => {
+            console.log(error);
+        })
+
+        window.location.reload();
+
+    }
+}
+
+form.addEventListener("submit",cadastrarUsuario);
+
+
+
+async function carregarBairros() {
+    var url = "http://localhost:5000/api/bairros";
+    await fetch(url)
+        .then(response => response.json())
+        .then(data => preencherBairros(data))
+        .catch(error => {
+            console.log(error);
+            alert("Ocorreu um erro inesperado. Tente novamente mais tarde.");
+            window.location.href = "admin.html";
+        })
+}
+
+
+preencherBairros = (bairros) => {
+    bairrosCadastrados = bairros;
+    bairros.forEach(item => {
+        var option = document.createElement("option");
+        option.value = item.idBairro;
+        option.label = item.nomeBairro;
+        option.className = "bairro_option";
+
+        selectBairro.appendChild(option);
+    })
+}
+
+$("#cep").change(() => {
+    let valor = $("#cep").val();
+
+    if (valor.length === 8 || valor.length === 9) {
+        try {
+            let caracteres = valor.split("");
+            let arrayCep = caracteres.filter(x => x != "-");
+            let cep = arrayCep.join("");
+
+            procurarCep(cep);
+
+        } catch (error) {
+            console.warn(error);
+        }
+    }
+})
+
+async function procurarCep(cep) {
+    try {
+
+        await $.getJSON("https://viacep.com.br/ws/" + cep + "/json/?callback=?", function (dados) {
+
+            if (!("erro" in dados)) {
+                //Atualiza os campos com os valores da consulta.
+                $("#logradouro").val(dados.logradouro);
+                mudarBairroSelecionado(dados.bairro);
+            } //end if.
+            else {
+                //CEP pesquisado não foi encontrado.
+                limpa_formulario_cep();
+                alert("CEP não encontrado.");
+            }
+        });
+
+    } catch (error) {
+        console.warn(error);
+    }
+}
+
+
+
+mudarBairroSelecionado = (nomeBairro) => {
+    bairroSelecionado = bairrosCadastrados.find(x => x.nomeBairro == nomeBairro);
+
+    if (bairroSelecionado !== null && bairroSelecionado !== undefined) {
+        $("#bairro").val(bairroSelecionado.idBairro);
+    }
+
+}
